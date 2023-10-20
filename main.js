379309658ba5a372b842bdae6a15f11bb7c7d8bb
profmariaizabellara <101982763+profmariaizabellara@users.noreply.github.com>
2023-10-20 14:26:48 -0300

    tec

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-function tocaSomPom() {
-    document.querySelector('.som_tecla_pom');
-    document.querySelector( ‘#som_tecla_pom’ ).play() ;
-}
+function tocaSomPom(){
+    document.querySelector(‘#som_tecla_pom’).play();
+    }

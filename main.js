3e0279485ccfb7c617bd14ef1ae3265ae40a2e4b
profmariaizabellara <101982763+profmariaizabellara@users.noreply.github.com>
2023-10-20 14:42:37 -0300

    teste

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 function tocaSomPom(){
-    document.querySelector('.tecla_pom').onclick = tocaSomPom;
+    document.querySelector('.tecla_pom'). onclick = tocaSomPom;
+    document.querySelector('#som_tecla_pom').play();
     }

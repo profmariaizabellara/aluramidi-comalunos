0d70bcc99e6a29ecc429859140a1dfc24d69d0d5
profmariaizabellara <101982763+profmariaizabellara@users.noreply.github.com>
2023-10-25 09:13:14 -0300

    outras teclas

--- main.js
+++ main.js
@@ -21,3 +21,45 @@ function tocaSomPuff() {
 }
 
 document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+function tocaSomSplash() {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+function tocaSomToim() {
+    document.querySelector('#som_tecla_toim').play();
+}
+
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
+
+function tocaSomPsh() {
+    document.querySelector('#som_tecla_psh').play();
+}
+
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+function tocaSomToim() {
+    document.querySelector('#som_tecla_toim').play();
+}
+
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
+
+function tocaSomPsh() {
+    document.querySelector('#som_tecla_psh').play();
+}
+
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+function tocaSomTic() {
+    document.querySelector('#som_tecla_tic').play();
+}
+
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+function tocaSomTom() {
+    document.querySelector('#som_tecla_tom').play();
+}
+
+document.querySelector('.tecla_tom').onclick = tocaSomTom;

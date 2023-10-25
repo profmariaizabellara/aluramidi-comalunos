02228bd1d64f633d6351432e3687cd4cd0dd026c
profmariaizabellara <101982763+profmariaizabellara@users.noreply.github.com>
2023-10-25 08:59:32 -0300

    clap

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ function tocaSomPom(){
     }
     
     document.querySelector('.tecla_pom'). onclick = tocaSomPom;
+
+    function tocaSomClap(){
+        document.querySelector('#som_tecla_clap').play();
+        }
+        
+        document.querySelector('.tecla_clap'). onclick = tocaSomClap;
